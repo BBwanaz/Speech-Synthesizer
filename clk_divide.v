db2f6afb9daad3e1e4d0b69a76677a167564fa3b
Brandon Bwanakocha <38164088+BBwanaz@users.noreply.github.com>
2020-12-21 21:58:22 -0800

    Update clk_divide.v

--- clk_divide.v
+++ clk_divide.v
@@ -8,21 +8,14 @@ module clk_divide(clk_input, divisor,clk_output);
 	
 	reg [31:0] counter = 32'd0;
 	
-	//this line doesn't work for very short counter timings
-	//assign clk_output = (counter > divisor/2)?1'd1:1'd0; //if(count>divisor/2) = half the cycle
-	
 	always @(posedge clk_input)
 		begin
-			counter = counter + 1'd1; //increment counter
-			if(counter >= divisor/2-1) //at half the cycle
+			counter = counter + 1'd1; 			//increment counter
+			if(counter >= divisor/2-1) 			//at half the cycle
 				begin
-					counter = 1'd0; //reset counter after full cycle
-					clk_output = ~clk_output; //flip the clock
+					counter = 1'd0; 		//reset counter after full cycle
+					clk_output = ~clk_output; 	//flip the clock
 				end
-			
-			
 		end	
-	
-	
 endmodule
 
